ec7190574de00601ad4a1bec621c13e2bb9b9da7
shamratneero <107503844+shamratneero@users.noreply.github.com>
2026-06-27 02:21:02 +0600

    add train and val transforms, tested successfully

---
+++ src/data/transforms.py
@@ -0,0 +1,23 @@
+import torchvision.transforms as transforms
+
+def get_train_transforms(image_size=224):
+    return transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.RandomRotation(degrees=5),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+
+def get_val_transforms(image_size=224):
+    return transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
